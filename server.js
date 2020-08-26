765d632a8886250ad3b72bf1f1e4dff242c62439
Neeraj Patel <neerajhpatel@gmail.com>
2020-08-26 15:24:03 +1000

    Modified directory and modified root package.json for deployment

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const server = express();
+
+// Used to parse the request body
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+// Import routes
+const userRoute = require('./server/Routes/user.js');
+
+// Connect to database
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+  console.log('connected to DB!')
+);
+
+// Assign routes
+server.use('/user', userRoute);
+
+server.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});

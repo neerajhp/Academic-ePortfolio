34ff98a1f14ea4499c8077cc79b472bd16ef90b2
Polo Chiu <pchiu2@student.unimelb.edu.au>
2020-08-29 15:20:21 +1000

    basic login and register function (no email auth and no cookies)

--- server/Models/User.js
+++ server/Models/User.js
@@ -3,10 +3,8 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
-    email: String,
-    birthDate: Date,
-    // This is just for testing
-    university: String
+    email: {type: String, required: true},
+    password: {type: String, required: true},
 
 })
 
--- server/Routes/user.js
+++ server/Routes/user.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../Models/User.js");
+var bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 // For signup
 // Maybe sign up might need its own route instead
@@ -13,36 +15,71 @@ router.get("/signup", (req, res) => {
     res.send("You are in the signup page");
 });
 
-router.post("/signup", (req, res) => {
-    const newUser = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        birthDate: req.body.birthDate,
-        university: req.body.university
-    });
+router.post("/signup", async (req, res) => {
 
-    console.log(newUser.firstName);
-    console.log(newUser.lastName);
+    bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
 
-    newUser.save()
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
-            res.json({message: err});
+        const newUser = new User({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: hash,
+
         })
 
-    console.log("User added")
+        console.log(newUser.firstName);
+        console.log(newUser.lastName);
+        console.log(newUser.email);
+        console.log(newUser.password);
+        console.log("User added");
 
-    // res.send(req.body)
-    // Call on a sign up function from the user controller
+        await User.findOne({
+            email: newUser.email
+        })
+        .then(async profile => {
+            if(!profile){
+              newUser.save()
+              res.send("User added");
+            } else{
+              res.send("User already exists...");
+            }
+        })
+    })
 });
 
 // For login
 router.get("/login", (req, res) => {
     res.send("Click here to login");
     // Call on a login function from the user controller
+});
+
+router.post("/login", async (req, res) => {
+    var newUser = {};
+    newUser.email = req.body.email;
+    newUser.password = req.body.password;
+
+    //test if password matches 
+    await User.findOne({
+            email: newUser.email
+        })
+        .then(profile => {
+            if (!profile) {
+                res.send("User not exist");
+            } else {
+                bcrypt.compare(req.body.password, profile.password, function(err, result) {
+                    // res.send(newUser.password);
+                    // res.send(req.body.password);
+                    if (result == true) {
+                        res.send("User authenticated");
+                    } else {
+                        res.send("incorrect password!");
+                    }
+                });
+            }
+        })
+        .catch(err => {
+            console.log("Error is ", err.message);
+        });
 });
 
 module.exports = router;

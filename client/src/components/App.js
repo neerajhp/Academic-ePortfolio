717d1fc6727ec53b175cfd179e916e879095ecc6
Neeraj Patel <neerajhpatel@gmail.com>
2020-08-31 15:00:09 +1000

    Ammended App.js for Routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import ProfilePage from './ProfilePage';
 import LoginPage from './LoginPage';
 
 function App() {
   return (
     <div>
-      <ProfilePage />
+      <Router>
+        <Switch>
+          <Route exact path='/' component={LoginPage} />
+          <Route path='/profile' component={ProfilePage} />
+        </Switch>
+      </Router>
     </div>
   );
 }

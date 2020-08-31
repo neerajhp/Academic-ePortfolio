38a6aa47e3823acca8950725061f1245268b5039
Polo Chiu <pchiu2@student.unimelb.edu.au>
2020-08-31 10:33:41 +1000

    transferred into controllers

--- server/Routes/user.js
+++ server/Routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const userController = require("../Controllers/user.js");
 const User = require("../Models/User.js");
 var bcrypt = require("bcrypt");
 const saltRounds = 10;
@@ -15,71 +16,16 @@ router.get("/signup", (req, res) => {
     res.send("You are in the signup page");
 });
 
-router.post("/signup", async (req, res) => {
+router.post("/signup", async(req,res)=>{
+    userController.postSignup(req, res);
+})
 
-    bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
-
-        const newUser = new User({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            password: hash,
-
-        })
-
-        console.log(newUser.firstName);
-        console.log(newUser.lastName);
-        console.log(newUser.email);
-        console.log(newUser.password);
-        console.log("User added");
-
-        await User.findOne({
-            email: newUser.email
-        })
-        .then(async profile => {
-            if(!profile){
-              newUser.save()
-              res.send("User added");
-            } else{
-              res.send("User already exists...");
-            }
-        })
-    })
-});
-
-// For login
 router.get("/login", (req, res) => {
     res.send("Click here to login");
-    // Call on a login function from the user controller
 });
 
 router.post("/login", async (req, res) => {
-    var newUser = {};
-    newUser.email = req.body.email;
-    newUser.password = req.body.password;
-
-    //test if password matches 
-    await User.findOne({
-            email: newUser.email
-        })
-        .then(profile => {
-            if (!profile) {
-                res.send("User not exist");
-            } else {
-                bcrypt.compare(req.body.password, profile.password, function(err, result) {
-                    // res.send(newUser.password);
-                    // res.send(req.body.password);
-                    if (result == true) {
-                        res.send("User authenticated");
-                    } else {
-                        res.send("incorrect password!");
-                    }
-                });
-            }
-        })
-        .catch(err => {
-            console.log("Error is ", err.message);
-        });
-});
+  userController.postLogin(req, res);
+})
 
 module.exports = router;

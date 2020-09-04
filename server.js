5093442ef1af682974f567b59b08144958988317
Neeraj Patel <neerajhpatel@gmail.com>
2020-09-04 14:59:37 +1000

    Debugging deployment

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ if (
   process.env.NODE_ENV === 'production' ||
   process.env.NODE_ENV === 'staging'
 ) {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
+  server.use(express.static('client/build'));
+  server.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
 }

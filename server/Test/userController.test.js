90296234984aef88361daf04a3c304d96b5e264f
LKY1999 <30360639+LKY1999@users.noreply.github.com>
2020-09-20 10:17:50 +1000

    Test connected to regular server

---
+++ server/Test/userController.test.js
@@ -0,0 +1,22 @@
+const server = require('../../server') // Link to your server file
+const supertest = require('supertest')
+const request = supertest(server)
+
+
+test("Should return signup page", async () =>{
+    await request.get("/api/user/signup")
+    .expect(200)
+
+})
+
+test("Should sign up a user", async () =>{
+    await request.post("/api/user/signup")
+    .send({
+        firstName: "test",
+        lastName: "test",
+        email: "test@gmail.com",
+        password: "test123"
+    })
+    .expect(200)
+
+})

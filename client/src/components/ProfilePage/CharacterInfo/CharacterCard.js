0c474f0d29d47e57c8c968cde834d4274db24876
田瑾欣 <tianjinxin@tianjinindeAir2.modem>
2020-09-27 15:00:08 +1000

    upload function

--- client/src/components/ProfilePage/CharacterInfo/CharacterCard.js
+++ client/src/components/ProfilePage/CharacterInfo/CharacterCard.js
@@ -1,41 +1,53 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Paper, Avatar, Typography } from '@material-ui/core';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Paper, Avatar, Typography } from "@material-ui/core";
+import { Upload } from "antd";
 
 /* ================ Styling ================ */
 const useStyles = makeStyles((theme) => ({
   characterCard: {
-    margin: '0 0 1% 1%',
-    width: '100%',
+    margin: "0 0 1% 1%",
+    width: "100%",
     background: theme.palette.primary.light,
-    padding: '5%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    padding: "5%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
   profilePicture: {
-    height: '5em',
-    width: '5em',
+    height: "5em",
+    width: "5em",
+    cursor: "pointer",
   },
   bio: {
-    marginLeft: '5%',
+    marginLeft: "5%",
     flexGrow: 1,
-    color: 'white !important ',
+    color: "white !important ",
   },
 }));
 /* ================ Component ================ */
 const CharacterCard = ({ user }) => {
   const classes = useStyles();
   return (
-    <Paper className={classes.characterCard}>
-      <Avatar className={classes.profilePicture} />
-      <div className={classes.bio}>
-        <Typography variant='h2'>
-          {user.firstName} {user.lastName}
-        </Typography>
-        <Typography>{user.bio}</Typography>
-      </div>
-    </Paper>
+      <Paper className={classes.characterCard}>
+        <Upload
+            name="profile-pic"
+            accept="image/*"
+            showUploadList={false}
+            action="/api/upload/profile-pic"
+            headers={{
+              Authorization: "Bearer: " + JSON.parse(localStorage.getItem("token")),
+            }}
+        >
+          <Avatar className={classes.profilePicture} />
+        </Upload>
+        <div className={classes.bio}>
+          <Typography variant="h2">
+            {user.firstName} {user.lastName}
+          </Typography>
+          <Typography>{user.bio}</Typography>
+        </div>
+      </Paper>
   );
 };
 
--- client/src/components/ProfilePage/ReflectionInfo/ReflectionCard.js
+++ client/src/components/ProfilePage/ReflectionInfo/ReflectionCard.js
@@ -1,36 +1,94 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Paper, Typography } from '@material-ui/core';
+import React, { useRef, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Paper, Typography } from "@material-ui/core";
+import { Button, message } from "antd";
+import axios from "axios";
 
 /* ================ Styling ================ */
 const useStyles = makeStyles((theme) => ({
   card: {
-    margin: '0 0 1% 1%',
-    width: '100%',
+    margin: "0 0 1% 1%",
+    width: "100%",
     background: theme.palette.primary.light,
     color: theme.palette.text.secondary,
-    padding: '5%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    padding: "5%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    position: "relative",
   },
   description: {
-    marginLeft: '5%',
-    color: 'white !important ',
+    marginLeft: "5%",
+    color: "white !important ",
+  },
+  upload: {
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+  },
+  hidden: {
+    display: "none",
   },
 }));
 /* ================ Component ================ */
 const ReflectionCard = () => {
   const classes = useStyles();
+  const inputEl = useRef(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleChoseImg = (e) => {
+    e.preventDefault();
+    const file = e.target.files;
+    if (!file.length) {
+      return false;
+    }
+    if (file.length < 6) {
+      setLoading(true);
+      let param = new FormData();
+      for (const key in file) {
+        if (file.hasOwnProperty(key)) {
+          const item = file[key];
+          param.append("image[]", item);
+        }
+      }
+      axios({
+        method: "post",
+        url: "/api/upload/images",
+        data: param,
+        headers: {
+          Authorization: "Bearer: " + JSON.parse(localStorage.getItem("token")),
+        },
+        responseType: "blob",
+      }).then((result) => {
+        setLoading(false);
+      });
+    } else {
+      message.info("the max number is 5");
+    }
+  };
+
   return (
-    <Paper className={classes.card}>
-      <div className={classes.bio}>
-        <Typography className={classes.title} variant='h2'>
-          This is a Reflection
-        </Typography>
-        <Typography>This is the blog introduction</Typography>
-      </div>
-    </Paper>
+      <Paper className={classes.card}>
+        <div className={classes.bio}>
+          <Typography className={classes.title} variant="h2">
+            This is a Reflection
+          </Typography>
+          <Typography>This is the blog introduction</Typography>
+          <input
+              className={classes.hidden}
+              type="file"
+              ref={inputEl}
+              accept="image/*"
+              multiple
+              onChange={handleChoseImg}
+          />
+          <div className={classes.upload}>
+            <Button loading={loading} onClick={() => inputEl.current.click()}>
+              Upload
+            </Button>
+          </div>
+        </div>
+      </Paper>
   );
 };
 

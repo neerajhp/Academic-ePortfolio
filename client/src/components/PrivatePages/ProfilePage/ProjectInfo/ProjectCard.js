ab4c509f9a1649a307f8b4676b61783a40d1c8e8
JinxinT <54254194+JinxinT@users.noreply.github.com>
2020-10-02 20:20:07 +1000

    dowmload function

--- client/src/components/PrivatePages/ProfilePage/ProjectInfo/ProjectCard.js
+++ client/src/components/PrivatePages/ProfilePage/ProjectInfo/ProjectCard.js
@@ -1,8 +1,10 @@
 import React, { useRef, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Paper, Typography } from "@material-ui/core";
-import { Button, message } from "antd";
+import { Button, message, Form, Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
 import axios from "axios";
+import API from "../../../../api/API";
 
 /* ================ Styling ================ */
 
@@ -48,6 +50,8 @@ const ProjectCard = ({ type }) => {
   const classes = useStyles();
   const inputEl = useRef(null);
   const [loading, setLoading] = useState(false);
+  const [allFiles, setAllFiles] = useState([]);
+  const [file, setFile] = useState("");
   //Default  card size is large
   var cardSize;
 
@@ -77,7 +81,7 @@ const ProjectCard = ({ type }) => {
       for (const key in file) {
         if (file.hasOwnProperty(key)) {
           const item = file[key];
-          param.append("document[]", item);
+          param.append("image", item);
         }
       }
       axios({
@@ -96,32 +100,106 @@ const ProjectCard = ({ type }) => {
     }
   };
 
+  const onFinish = (values) => {
+    API.getAllFiles(values).then((result) => {
+      if (result.status === 200) {
+        setAllFiles(result.data);
+      }
+    });
+  };
+
+  const onIdFinish = (values) => {
+    API.getFile(values).then((result) => {
+      if (result.status === 200) {
+        setFile(result.data);
+      }
+    });
+  };
+
   return (
-      <Paper className={`${classes.card}  ${cardSize}`}>
-        <div className={classes.bio}>
-          <Typography className={classes.title} variant="h2">
-            This is a {type} Project Card
-          </Typography>
-          <Typography>This is the project description</Typography>
-          {type === "small" && (
-              <>
-                <input
-                    className={classes.hidden}
-                    type="file"
-                    ref={inputEl}
-                    accept=".PDF,.png,.jpeg.JPEG,.pdf,.mp4,.MP4,.DOCX,.docx"
-                    multiple
-                    onChange={handleChoseFile}
-                />
-                <div className={classes.upload}>
-                  <Button loading={loading} onClick={() => inputEl.current.click()}>
-                    Upload
-                  </Button>
-                </div>
-              </>
-          )}
-        </div>
-      </Paper>
+    <Paper className={`${classes.card}  ${cardSize}`}>
+      <div className={classes.bio}>
+        <Typography className={classes.title} variant="h2">
+          This is a {type} Project Card
+        </Typography>
+        <Typography>This is the project description</Typography>
+        {type === "small" && (
+          <>
+            <input
+              className={classes.hidden}
+              type="file"
+              ref={inputEl}
+              accept=".PDF,.png,.jpeg.JPEG,.pdf,.mp4,.MP4,.DOCX,.docx"
+              multiple
+              onChange={handleChoseFile}
+            />
+            <div className={classes.upload}>
+              <Button loading={loading} onClick={() => inputEl.current.click()}>
+                Upload
+              </Button>
+            </div>
+          </>
+        )}
+        {type === "large" && (
+          <>
+            <div style={{ marginTop: 20 }}>
+              <Form layout="inline" onFinish={onFinish}>
+                <Form.Item>search by name:</Form.Item>
+                <Form.Item
+                  name="document"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your document id!",
+                    },
+                  ]}
+                >
+                  <Input placeholder="document id" />
+                </Form.Item>
+                <Form.Item>
+                  <Button
+                    htmlType="submit"
+                    type="primary"
+                    shape="circle"
+                    icon={<SearchOutlined />}
+                  />
+                </Form.Item>
+              </Form>
+              <div>
+                {allFiles.map((item) => (
+                  <div key={item.id}>{item.fieldName}</div>
+                ))}
+              </div>
+            </div>
+            <div style={{ marginTop: 20 }}>
+              <Form layout="inline" onFinish={onIdFinish}>
+                <Form.Item style={{ marginLeft: 24 }}>search by id:</Form.Item>
+                <Form.Item
+                  name="id"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your document id!",
+                    },
+                  ]}
+                >
+                  <Input placeholder="document id" />
+                </Form.Item>
+                <Form.Item>
+                  <Button
+                    htmlType="submit"
+                    type="primary"
+                    shape="circle"
+                    icon={<SearchOutlined />}
+                  />
+                </Form.Item>
+              </Form>
+              <div>{file}</div>
+            </div>
+          </>
+        )}
+      </div>
+    </Paper>
   );
 };
 

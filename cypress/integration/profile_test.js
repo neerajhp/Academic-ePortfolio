05e8f3581a73d36bba776c7d82becab8ba8b9b91
Polo Chiu <pchiu2@student.unimelb.edu.au>
2020-10-16 02:35:36 +1100

    Updated some tests
    installed package to test react components

--- cypress/integration/profile_test.js
+++ cypress/integration/profile_test.js
@@ -1,18 +1,36 @@
 describe('Profile Page', () => {
+    beforeEach(() => {
+      //login first 
+      cy.visit('http://localhost:3000/home/login')
 
-  
+      //fill out the info and submit
+      cy.get('input').eq(0).type('cartman@email.com')
+      cy.get('input').eq(1).type('12345678')
+      cy.get('button[type=submit]').eq(0).click()
+      cy.url().should('eq', 'http://localhost:3000/myprofile')
+      cy.wait(500)
+
+  })
+
     it('test logout', () => {
       
-      //login first 
-      cy.visit('http://localhost:3000/login')
-      cy.get('input').first().type('cartman@email.com')
-      cy.get('input').last().type('12345678')
-      cy.get('button[type=submit]').click()
-      cy.url().should('eq', 'http://localhost:3000/profile')
-
       //click the logout button
       cy.get('button').first().click()
-      cy.url().should('eq', 'http://localhost:3000')
+      cy.url().should('eq', 'http://localhost:3000/home/login')
+
+    })
+
+    it('test access myaccount', () => {
+      cy.get('a[href*="/myaccount"]').click()
+      cy.url().should('eq', 'http://localhost:3000/myaccount')
+
+    })
+
+    it('test education form', () => {
+      cy.wait(500)
+      
+      //clikcing on the button on the professional work row
+      cy.get('button').eq(3).click()
 
     })
   })

844f07cac55848065df94f6a0314eb47806798bc
Neeraj Patel <neerajhpatel@gmail.com>
2020-10-20 00:47:17 +1100

    Removed capitalisation on 'Add ...' buttons

--- client/src/components/PrivatePages/ProfilePage/EducationInfo/EducationDialog.js
+++ client/src/components/PrivatePages/ProfilePage/EducationInfo/EducationDialog.js
@@ -32,6 +32,7 @@ const useStyles = makeStyles((theme) => ({
   },
   newEducationButton: {
     '&.MuiButton-text': {
+      textTransform: 'none',
       padding: `${theme.spacing(4)}px ${theme.spacing(2)}px`,
     },
     '&.MuiButtonBase-root': {

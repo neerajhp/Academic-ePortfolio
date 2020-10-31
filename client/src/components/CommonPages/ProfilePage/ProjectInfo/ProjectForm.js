b8831fe2792b02cd272190ea843476e14a52d2ef
Neeraj Patel <neerajhpatel@gmail.com>
2020-10-31 12:19:27 +1100

    Fixed add project bug

--- client/src/components/CommonPages/ProfilePage/ProjectInfo/ProjectForm.js
+++ client/src/components/CommonPages/ProfilePage/ProjectInfo/ProjectForm.js
@@ -13,7 +13,7 @@ import API from '../../../../api/API';
 import FormikField from '../../../utils/FormikField';
 import RTE from '../../../utils/RTE';
 import { DropzoneArea } from 'material-ui-dropzone';
-import axios from "axios";
+import axios from 'axios';
 import { Upload, message } from 'antd';
 
 /* ================ Styling ================ */
@@ -70,22 +70,22 @@ const useStyles = makeStyles((theme) => ({
 
 /* ================ Component ================ */
 
-const ProjectForm = ({ handleClose, records, newWork}) => {
+const ProjectForm = ({ handleClose, records, newWork }) => {
   const classes = useStyles();
 
   //RTE
   const [rteValue, setRTEValue] = useState([]);
 
- // const [attachedFiles, setAttachedFiles] = useState([]);
+  // const [attachedFiles, setAttachedFiles] = useState([]);
 
- // remove
+  // remove
   const handleDel = () => {
     API.removeFeaturedWork(records._id).then((result) => {
       if (result.status === 200) {
-        console.log(result);
+        handleClose();
       }
-    })
-    };
+    });
+  };
 
   useEffect(() => {
     if (records.description === undefined || records.description.length === 0) {
@@ -99,7 +99,7 @@ const ProjectForm = ({ handleClose, records, newWork}) => {
     setRTEValue(records.description);
   }, [records.description]);
 
-//upload files
+  //upload files
   const handleChooseFile = (e) => {
     e.preventDefault();
     const attachedFiles = e.target.files;
@@ -123,17 +123,17 @@ const ProjectForm = ({ handleClose, records, newWork}) => {
         },
         responseType: 'blob',
       }).then((result) => {});
-      API.uploadFiles(attachedFiles,records._id).then((result) => {
+      API.uploadFiles(attachedFiles, records._id).then((result) => {
         if (result.status === 200) {
           console.log(result);
         }
-      })
+      });
     }
   };
 
-//test console
-  const attachedFiles = new File(["foo"], "file", {
-    type: ".PDF,.png,.jpeg.JPEG,.pdf,.mp4,.MP4,.DOCX,.docx",
+  //test console
+  const attachedFiles = new File(['foo'], 'file', {
+    type: '.PDF,.png,.jpeg.JPEG,.pdf,.mp4,.MP4,.DOCX,.docx',
   });
 
   return (
@@ -155,14 +155,15 @@ const ProjectForm = ({ handleClose, records, newWork}) => {
         };
         if (newWork) {
           API.createFeaturedWork(featuredWork).then(({ res }) => {
+            console.log(res);
+            handleClose();
+          });
+        } else {
+          API.editFeaturedWork(featuredWork, records._id).then(({ res }) => {
             // console.log(res);
             handleClose();
           });
         }
-        API.editFeaturedWork(featuredWork, records._id).then(({ res }) => {
-          // console.log(res);
-          handleClose();
-        });
       }}
     >
       {(formikProps) => (
@@ -184,8 +185,10 @@ const ProjectForm = ({ handleClose, records, newWork}) => {
             <div className={classes.dropzoneContainer}>
               <DropzoneArea
                 attachedFiles={attachedFiles}
-                acceptedFiles={['.PDF,.png,.jpeg.JPEG,.pdf,.mp4,.MP4,.DOCX,.docx']}
-                dropzoneText={"Drag and drop an image here or click"}
+                acceptedFiles={[
+                  '.PDF,.png,.jpeg.JPEG,.pdf,.mp4,.MP4,.DOCX,.docx',
+                ]}
+                dropzoneText={'Drag and drop an image here or click'}
                 filesLimit={5}
                 onChange={(files) => console.log('Files:', files)}
               />
@@ -203,10 +206,10 @@ const ProjectForm = ({ handleClose, records, newWork}) => {
                   <Typography>Delete</Typography>
                 </Button>
                 {formikProps.isSubmitting && (
-                    <CircularProgress
-                        size={24}
-                        className={classes.buttonProgress}
-                    />
+                  <CircularProgress
+                    size={24}
+                    className={classes.buttonProgress}
+                  />
                 )}
               </div>
               <div className={classes.buttonWrapper}>

3f92821900634abb295fe26f33d49b7f383068f7
Polo Chiu <polochiu23@gmail.com>
2020-11-20 06:45:21 +1100

    Revert "Revert "tests updates""
    This reverts commit 27bb7bb27594e9bf48d35dbb02d487ad492d1c65.

--- cypress/integration/landing_test.js
+++ cypress/integration/landing_test.js
@@ -1,15 +1,49 @@
-describe("landing page", () => {
-  it("login button redirects correctly", () => {
-    cy.visit("http://localhost:3000");
-    cy.contains("Login").click();
-    //expect to redirect to login page
-    cy.url().should("eq", "http://localhost:3000/login");
-  });
+describe('landing page', () => {
 
-  it("signup button redirects correctly", () => {
-    cy.visit("http://localhost:3000");
-    cy.contains("Create").click();
-    //expect to redirect to signup page
-    cy.url().should("eq", "http://localhost:3000/signup");
-  });
-});
+  //test we can go to the login page 
+  it('test we can go to the login page', () => {
+
+    //check we are on the landing page
+    cy.visit('http://localhost:3000')
+    cy.url().should('eq', 'http://localhost:3000/home/landing')
+
+    //click the login page button 
+    cy.get('button').eq(0).click()
+
+    // expect to redirect to login page
+    cy.url().should('eq', 'http://localhost:3000/home/login')
+  })
+
+  it('test we can go to the signup page', () => {
+
+    //check we are on the landing page
+    cy.visit('http://localhost:3000')
+    cy.url().should('eq', 'http://localhost:3000/home/landing')
+
+    //click the login page button 
+    cy.get('button').eq(0).click()
+
+    // expect to redirect to login page
+    cy.url().should('eq', 'http://localhost:3000/home/login')
+
+    //click the signup page button
+    cy.get('button').eq(1).click()
+
+    cy.url().should('eq', 'http://localhost:3000/home/signup')
+
+  })
+  
+  //test we can go to the signup page 
+  it('signup button redirects correctly', () => {
+
+      //check we are on the landing page
+      cy.visit('http://localhost:3000')
+      cy.url().should('eq', 'http://localhost:3000/home/landing')
+
+      //click search page button
+      cy.get('button').eq(1).click()
+
+      //expect to redirect to search page
+      cy.url().should('eq', 'http://localhost:3000/home/search')
+  })
+})
